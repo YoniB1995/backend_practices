0b2211d2d781f3921f171757ae0e6ebc68960145
Yoni <yonatansamfisher@gmail.com>
2021-07-28 10:28:24 +0300

    updates

--- database/mySQL.js
+++ database/mySQL.js
@@ -2,26 +2,27 @@
 const mysql = require('mysql')
 require('dotenv').config();
 
+const createPollConnection = () => {
 try{
-const pool = mysql.createPool({
+    return mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: '',
     database: process.env.DB_DATABASE
 });
-
-pool.query('SELECT * FROM `Persons`',(error,results,fields) => {
-    if (error) throw error;
-    console.log(results);
-})
-
-module.exports = pool
 }
 catch(error){
-    console.log("the pool is not connected , please try again")
+    console.log(error)
+}
 }
 
+module.exports = createPollConnection;
 
+
+// pool.query('SELECT * FROM `Persons`',(error,results,fields) => {
+//     if (error) throw error;
+//     console.log(results);
+// })
 
 
